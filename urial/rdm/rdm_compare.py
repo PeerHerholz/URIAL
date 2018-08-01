efb44df1a79c2fcd3475762a977b0f0c0a09c908
Peer Herholz <herholz.peer@gmail.com>
2018-08-01 15:47:02 -0700

    update rdm_compare.py
    - fix computation errors
    - add 2nd order computation between group average and models

--- urial/rdm/rdm_compare.py
+++ urial/rdm/rdm_compare.py
@@ -9,24 +9,34 @@ def rdm_compare(rdms, models, comp=None, plot=None):
     import pandas as pd
     from collections import OrderedDict
     from scipy.spatial import distance
+    from nilearn.connectome import sym_matrix_to_vec, vec_to_sym_matrix
     from scipy.stats import rankdata, spearmanr, kendalltau, pearsonr, mstats
     import numpy as np
     from itertools import combinations
+    import pickle
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import copy
+
+    class DefaultListOrderedDict(OrderedDict):
+        def __missing__(self,k):
+            self[k] = []
+            return self[k]
 
     if isinstance(rdms, str) is True:
         with open(rdms, 'rb') as f:
             dict_rdms = pickle.load(f)
-
-        rdms = dict_rdms['rdm']
-        conds = rdms[0].keys()
+        target_rdms = copy.deepcopy(dict_rdms['rdm'])
+        target_conds = target_rdms[0].keys()
     else:
-        rdms = rdms
-        conds = rdms[0].keys()
+        target_rdms = rdms
+        target_conds = rdms[0].keys()
 
     if isinstance(models, str) is True:
         with open(models, 'rb') as f:
             dict_models = pickle.load(f)
             models = dict_models['rdm']
+            model_ids = dict_models['id']
     else:
         models = models
 
@@ -35,47 +45,87 @@ def rdm_compare(rdms, models, comp=None, plot=None):
             del rdm['Unnamed: 0']
 
 
-    for index, rdm in enumerate(rdms):
-        rdms[index] = rdms[index].as_matrix()
-
+    for index, rdm in enumerate(target_rdms):
+        target_rdms[index] = target_rdms[index].as_matrix()
 
     global rdm_avg
 
-    rdm_avg = pd.DataFrame(np.mean(rdms, axis=0), columns=conds)
+    list_cor_rdm=list(range(0,len(target_rdms)))
+    list_p=list(range(0,len(target_rdms)))
+    target_rdms_trans=list(range(0,len(target_rdms)))
 
-    list_cor_rdm=list(range(0,len(rdms)))
-    list_p=list(range(0,len(rdms)))
+    if comp is None:
+        print('rdm values will not be transformed')
 
-    if comp is None or 'spearman':
-        for index, rdm in enumerate(rdms):
-        rdms[index]=rankdata(rdm)
-    elif comp=='pearson':
-        for index, rdm in enumerate(rdms):
-        rdms[index]=mstats.zscore(rdm)
+        rdm_avg = pd.DataFrame(np.mean(target_rdms, axis=0), columns=conds)
 
-    for index, part_rdm in enumerate(rdms):
-        list_cor_rdm[index], list_p[index] = spearmanr(part_rdm.flatten() , rdm_avg.as_matrix().flatten())
+        for index, part_rdm in enumerate(target_rdms):
+            list_cor_rdm[index], list_p[index] = kendalltau(part_rdm.flatten(), rdm_avg.as_matrix().flatten())
+
+        list_cor_sub = list()
+        list_cor_rdm_sub = list()
+        list_p_sub = list()
+
+        for index, part in enumerate(target_rdms):
+            tmp_rdms = target_rdms.copy()
+            tmp_part = target_rdms[index]
+            tmp_rdms.pop(index)
+            tmp_rdm_avg = np.mean(tmp_rdms, axis=0)
+            list_cor_sub.append(kendalltau(tmp_part.flatten(), tmp_rdm_avg.flatten()))
+
+        for i, cor in enumerate(list_cor_sub):
+            list_cor_rdm_sub.append(cor.correlation)
+            list_p_sub.append(cor.pvalue)
+
+    elif comp == 'spearman':
+        for index, rdm in enumerate(target_rdms):
+
+            target_rdms_trans[index] = vec_to_sym_matrix(rankdata(sym_matrix_to_vec(rdm)))
+            rdm_avg = pd.DataFrame(np.mean(target_rdms_trans, axis=0), columns=conds)
+
+        for index, part_rdm in enumerate(target_rdms_trans):
+            list_cor_rdm[index], list_p[index] = spearmanr(part_rdm.flatten(), rdm_avg.as_matrix().flatten())
+
+        list_cor_sub = list()
+        list_cor_rdm_sub = list()
+        list_p_sub = list()
+
+        for index, part in enumerate(target_rdms_trans):
+            tmp_rdms = target_rdms_trans.copy()
+            tmp_part = target_rdms_trans[index]
+            tmp_rdms.pop(index)
+            tmp_rdm_avg = np.mean(tmp_rdms, axis=0)
+            list_cor_sub.append(spearmanr(tmp_part.flatten(), tmp_rdm_avg.flatten()))
+
+        for i, cor in enumerate(list_cor_sub):
+            list_cor_rdm_sub.append(cor.correlation)
+            list_p_sub.append(cor.pvalue)
+
+    elif comp == 'pearson':
+        for index, rdm in enumerate(target_rdms):
+
+            target_rdms_trans[index] = vec_to_sym_matrix(mstats.zscore(sym_matrix_to_vec(rdm)))
+            rdm_avg = pd.DataFrame(np.mean(target_rdms_trans, axis=0), columns=conds)
+
+        for index, part_rdm in enumerate(target_rdms_trans):
+            list_cor_rdm[index], list_p[index] = pearsonr(part_rdm.flatten(), rdm_avg.as_matrix().flatten())
+
+        list_cor_sub = list()
+        list_cor_rdm_sub = list()
+        list_p_sub = list()
+
+        for index, part in enumerate(target_rdms_trans):
+            tmp_rdms = target_rdms_trans.copy()
+            tmp_part = target_rdms_trans[index]
+            tmp_rdms.pop(index)
+            tmp_rdm_avg = np.mean(tmp_rdms, axis=0)
+            list_cor_sub.append(pearsonr(tmp_part.flatten(), tmp_rdm_avg.flatten()))
+
+        for i, cor in enumerate(list_cor_sub):
+            list_cor_rdm_sub.append(cor[0])
+            list_p_sub.append(cor[1])
 
     upper_noise_ceiling = np.mean(list_cor_rdm)
-
-    list_cor_sub=list()
-    list_cor_rdm_sub=list()
-    list_p_sub=list()
-
-    for index, part in enumerate(rdms):
-        tmp_rdms=rdms.copy()
-        tmp_part=rdms[index]
-        tmp_rdms.pop(index)
-        tmp_rdm_avg=np.mean(tmp_rdms, axis=0)
-        list_cor_sub.append(spearmanr(tmp_part.flatten(), tmp_rdm_avg.flatten()))
-
-        #for i, p in enumerate(rdms):
-        #    list_cor_sub.append(spearmanr(p.flatten(), tmp_rdm_avg.flatten()))
-
-    for i,cor in enumerate(list_cor_sub):
-        list_cor_rdm_sub.append(cor.correlation)
-        list_p_sub.append(cor.pvalue)
-
     lower_noise_ceiling=np.mean(list_cor_rdm_sub)
 
     model_comp = pd.DataFrame(columns=['participant', 'models', 'cor'], index=np.arange(len(dict_models['id'])*len(dict_rdms['id'])))
@@ -84,19 +134,34 @@ def rdm_compare(rdms, models, comp=None, plot=None):
 
     list_cor_models=list()
 
-    if comp is None or 'spearman':
+    snd_rdms=list()
+    snd_rdms.append(rdm_avg.as_matrix())
+    for mod_rdm in models:
+        snd_rdms.append(mod_rdm.as_matrix())
+
+    ids_rdms=list()
+    ids_rdms.append('group average')
+    for mod_ids in model_ids:
+        ids_rdms.append(mod_ids)
+
+    if comp is None:
         for index, model_rdm in enumerate(dict_models['rdm']):
-            for i, sub_rdm in enumerate(dict_rdms['rdm']):
+            for i, sub_rdm in enumerate(target_rdms):
+            list_cor_models.append(kendalltau(sub_rdm.flatten(), model_rdm.as_matrix().flatten()).correlation)
+            rdms_dist = [kendalltau(x.flatten(), y.flatten()).correlation for x, y in combinations(snd_rdms, 2)]
+            rdms_dist = pd.DataFrame(distance.squareform(rdms_dist), columns=ids_rdms)
+    elif comp == 'spearman':
+        for index, model_rdm in enumerate(dict_models['rdm']):
+            for i, sub_rdm in enumerate(target_rdms_trans):
                 list_cor_models.append(spearmanr(sub_rdm.flatten(), model_rdm.as_matrix().flatten()).correlation)
+                rdms_dist = [spearmanr(x.flatten(), y.flatten()).correlation for x, y in combinations(snd_rdms, 2)]
+                rdms_dist = pd.DataFrame(distance.squareform(rdms_dist), columns=ids_rdms)
     elif comp == 'pearson':
         for index, model_rdm in enumerate(dict_models['rdm']):
-            for i, sub_rdm in enumerate(dict_rdms['rdm']):
+            for i, sub_rdm in enumerate(target_rdms_trans):
                 list_cor_models.append(pearsonr(sub_rdm.flatten(), model_rdm.as_matrix().flatten())[0])
-    elif comp == 'kendalltau':
-        for index, model_rdm in enumerate(dict_models['rdm']):
-            for i, sub_rdm in enumerate(dict_rdms['rdm']):
-            list_cor_models.append(kendalltau(sub_rdm.flatten(), model_rdm.as_matrix().flatten()).correlation)
-
+                rdms_dist = [pearsonr(x.flatten(), y.flatten())[0] for x, y in combinations(snd_rdms, 2)]
+                rdms_dist = pd.DataFrame(distance.squareform(rdms_dist), columns=ids_rdms)
 
     model_comp['cor']=list_cor_models
 
@@ -133,6 +198,4 @@ def rdm_compare(rdms, models, comp=None, plot=None):
         ax.add_patch(rect)
         plt.tight_layout()
 
-
-
-    return model_comp
+    return rdm_avg, model_comp, rdms_dist
