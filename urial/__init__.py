ab57032797c98c13feb3bac03234db250a1b4bb2
PeerHerholz <herholz.peer@gmail.com>
2019-06-13 12:22:04 +0200

    initial tests of docs

--- urial/__init__.py
+++ urial/__init__.py
@@ -1,1 +1,2 @@
 __all__ = ['plotting', 'rdm', 'utils']
+__version__ = '0.1.0'
--- urial/utils/__init__.py
+++ urial/utils/__init__.py
@@ -1,3 +1,5 @@
 from .rdm_mat2csv import  rdm_mat2csv
 from .rdm_concat_df import  rdm_concat_df
 __all_ = ['rdm_concat_df', 'rdm_mat2csv']
+
+__version__ = '0.0.1'
